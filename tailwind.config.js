e7674ca858d04f20ce5715dbd7c880349d9486b7
Bruv-dev <tonyoguamas@gmail.com>
2026-07-06 15:45:05 +0100

    ...

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,6 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['"Inter"', 'sans-serif'],
-        display: ['"Area Extended"', 'sans-serif'],
-        mono: ['"JetBrains Mono"', 'monospace'],
-      },
-      colors: {
-        'brand-primary': '#03303A',
-        'brand-light': '#F4F7F6',
-        'brand-accent': '#E2A855',
-      },
-      spacing: {
-        '4.5': '1.125rem', // 18px
-        '6.5': '1.625rem', // 26px
-      }
-    },
   },
   plugins: [
     require('tailwindcss-animate'),
